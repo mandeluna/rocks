62d56f0a3a87a2dc83da4cc92179dc0bed05b148
Steve Wart <steve@wart.ca>
2019-01-20 17:16:48 -0800

    Initial commit

---
+++ Rocks for Food/Biome.cs
@@ -0,0 +1,158 @@
+﻿using System;
+
+namespace Rocks_for_Food
+{
+    struct BiomeCell
+    {
+        public double Elevation;
+        public double Moisture;
+        public double Geology;
+    }
+
+    class Biome
+    {
+        readonly BiomeCell[,] cells;
+        readonly int height;
+        readonly int width;
+        FastNoise gen1, gen2, gen3;
+
+        public enum Climate
+        {
+            OCEAN, BEACH,
+            SCORCHED, BARE, TUNDRA, SNOW,
+            TEMPERATE_DESERT, SHRUBLAND, TAIGA,
+            GRASSLAND, TEMPERATE_DECIDUOUS_FOREST, TEMPERATE_RAIN_FOREST,
+            SUBTROPICAL_DESERT, TROPICAL_SEASONAL_FOREST, TROPICAL_RAIN_FOREST
+        }
+
+        public enum Geology
+        {
+            SAND, GRAVEL, SALT, TIN, COPPER, IRON, SILVER, GOLD
+        }
+
+        public Biome(int width, int height)
+        {
+            this.height = height;
+            this.width = width;
+            cells = new BiomeCell[width, height];
+            Generate();
+        }
+
+        public BiomeCell this[int x, int y]
+        {
+            get
+            {
+                return cells[x, y];
+            }
+        }
+
+        public Climate ClimateType(int x, int y)
+        {
+            double e = cells[x, y].Elevation;
+            double m = cells[x, y].Moisture;
+
+            if (e < 0.1) return Climate.OCEAN;
+            if (e < 0.12) return Climate.BEACH;
+
+            if (e > 0.8)
+            {
+                if (m < 0.1) return Climate.SCORCHED;
+                if (m < 0.2) return Climate.BARE;
+                if (m < 0.5) return Climate.TUNDRA;
+                return Climate.SNOW;
+            }
+
+            if (e > 0.6)
+            {
+                if (m < 0.33) return Climate.TEMPERATE_DESERT;
+                if (m < 0.66) return Climate.SHRUBLAND;
+                return Climate.TAIGA;
+            }
+
+            if (e > 0.3)
+            {
+                if (m < 0.16) return Climate.TEMPERATE_DESERT;
+                if (m < 0.50) return Climate.GRASSLAND;
+                if (m < 0.83) return Climate.TEMPERATE_DECIDUOUS_FOREST;
+                return Climate.TEMPERATE_RAIN_FOREST;
+            }
+
+            if (m < 0.16) return Climate.SUBTROPICAL_DESERT;
+            if (m < 0.33) return Climate.GRASSLAND;
+            if (m < 0.66) return Climate.TROPICAL_SEASONAL_FOREST;
+            return Climate.TROPICAL_RAIN_FOREST;
+        }
+
+        public Geology GeologyType(int x, int y)
+        {
+            double g = cells[x, y].Geology;
+
+            // each is less likely than the next SAND, GRAVEL, MARBLE, GRANITE, SALT, TIN, COPPER, IRON, SILVER, GOLD
+
+            if (g > 0.98) return Geology.GOLD;
+            if (g > 0.95) return Geology.SILVER;
+            if (g > 0.90) return Geology.IRON;
+            if (g > 0.85) return Geology.COPPER;
+            if (g > 0.80) return Geology.TIN;
+            if (g > 0.75) return Geology.SALT;
+            if (g > 0.50) return Geology.GRAVEL;
+
+            return Geology.SAND;
+        }
+
+        // convert range of noise values from [-1.0, 1.0] to [0, 1.0]
+        private double Normalize(double noiseValue)
+        {
+            return noiseValue / 2.0 + 0.5;
+        }
+
+        private void Generate()
+        {
+            int seed1 = 317955;
+            Random rng = new Random(seed1);
+            gen1 = new FastNoise(rng.Next());
+            gen2 = new FastNoise(rng.Next());
+            gen3 = new FastNoise(rng.Next());
+            gen1.SetFrequency(1.00f);
+            gen2.SetFrequency(3.00f);
+            gen3.SetFrequency(11.0f);
+
+            for (var y = 0; y < height; y++)
+            {
+                for (var x = 0; x < width; x++)
+                {
+                    float nx = x / (float)width + 0.5f;
+                    float ny = y / (float)height + 0.5f;
+
+                    var e = (1.00 * Normalize(gen1.GetNoise(1.0f * nx, 1.0f * ny))
+                           + 0.50 * Normalize(gen1.GetNoise(2.0f * nx, 2.0f * ny))
+                           + 0.25 * Normalize(gen1.GetNoise(4.0f * nx, 4.0f * ny))
+                           + 0.13 * Normalize(gen1.GetNoise(8.0f * nx, 8.0f * ny))
+                           + 0.06 * Normalize(gen1.GetNoise(16.0f * nx, 16.0f * ny))
+                           + 0.03 * Normalize(gen1.GetNoise(32.0f * nx, 32.0f * ny)));
+                    e /= (1.00 + 0.50 + 0.25 + 0.13 + 0.06 + 0.03);
+                    e = Math.Pow(e, 2.75);
+                    var m = (1.00 * Normalize(gen2.GetNoise(1.0f * nx, 1.0f * ny))
+                           + 0.75 * Normalize(gen2.GetNoise(2.0f * nx, 2.0f * ny))
+                           + 0.33 * Normalize(gen2.GetNoise(4.0f * nx, 4.0f * ny))
+                           + 0.33 * Normalize(gen2.GetNoise(8.0f * nx, 8.0f * ny))
+                           + 0.33 * Normalize(gen2.GetNoise(16.0f * nx, 16.0f * ny))
+                           + 0.50 * Normalize(gen2.GetNoise(32.0f * nx, 32.0f * ny)));
+                    m /= (1.00 + 0.75 + 0.33 + 0.33 + 0.33 + 0.50);
+                    var g = (0.50 * Normalize(gen3.GetNoise(1.0f * nx, 1.0f * ny))
+                           + 0.50 * Normalize(gen3.GetNoise(2.0f * nx, 2.0f * ny))
+                           + 0.50 * Normalize(gen3.GetNoise(4.0f * nx, 4.0f * ny))
+                           + 0.50 * Normalize(gen3.GetNoise(8.0f * nx, 8.0f * ny))
+                           + 0.50 * Normalize(gen3.GetNoise(16.0f * nx, 16.0f * ny))
+                           + 0.50 * Normalize(gen3.GetNoise(32.0f * nx, 32.0f * ny)));
+                    g /= (0.50 + 0.50 + 0.50 + 0.50 + 0.50 + 0.50);
+
+                    cells[x, y].Elevation = e;
+                    cells[x, y].Moisture = m;
+                    cells[x, y].Geology = g;
+                }
+            }
+
+        }
+    }
+}
--- Rocks for Food/Game1.cs
+++ Rocks for Food/Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace Rocks_for_Food
 {
@@ -11,6 +12,20 @@ namespace Rocks_for_Food
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        SpriteFont font;
+        Biome biome;
+        Texture2D biomeTexture, geologyTexture;
+
+        readonly int width = 1024;
+        readonly int height = 1024;
+
+        readonly double timeScale = 86400.0; // one second = one day of game time
+        DateTime worldTime = new DateTime(1, 1, 1, 0, 0, 0);
+
+        private MouseState previousMouseState;
+        private float zoomLevel = 1.0f;
+        private Vector2 panLevel = new Vector2(0, 0);
+        private bool showBiome = true;
         
         public Game1()
         {
@@ -26,9 +41,17 @@ namespace Rocks_for_Food
         /// </summary>
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
+
+            Console.WriteLine("Creating biome...");
+            biome = new Biome(width, height);
+            Console.WriteLine("...biome created");
+
+            biomeTexture = CreateBiomeTexture();
+            geologyTexture = CreateGeologyTexture();
+
+            this.IsMouseVisible = true;
+            previousMouseState = Mouse.GetState();
         }
 
         /// <summary>
@@ -40,7 +63,114 @@ namespace Rocks_for_Food
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
+            font = Content.Load<SpriteFont>("Status");
+        }
+
+        private Color BiomeColor(Biome.Geology type)
+        {
+            switch (type)
+            {
+                case Biome.Geology.SAND:
+                    return Color.Tan;
+                case Biome.Geology.GRAVEL:
+                    return Color.DarkGray;
+                case Biome.Geology.SALT:
+                    return Color.WhiteSmoke;
+                case Biome.Geology.TIN:
+                    return Color.Gray;
+                case Biome.Geology.COPPER:
+                    return Color.DarkSeaGreen;
+                case Biome.Geology.IRON:
+                    return Color.DarkRed;
+                case Biome.Geology.SILVER:
+                    return Color.Silver;
+                case Biome.Geology.GOLD:
+                    return Color.Gold;
+                default:
+                    return Color.Black;
+            }
+        }
+
+        private Color BiomeColor(Biome.Climate type)
+        {
+            switch (type)
+            {
+                case Biome.Climate.OCEAN:
+                    return Color.DarkBlue;
+                case Biome.Climate.BEACH:
+                    return Color.Tan;
+                case Biome.Climate.SCORCHED:
+                    return Color.DarkRed;
+                case Biome.Climate.BARE:
+                    return Color.Gray;
+                case Biome.Climate.TUNDRA:
+                    return Color.LightGray;
+                case Biome.Climate.SNOW:
+                    return Color.WhiteSmoke;
+                case Biome.Climate.TEMPERATE_DESERT:
+                    return Color.LightGoldenrodYellow;
+                case Biome.Climate.SHRUBLAND:
+                    return Color.LawnGreen;
+                case Biome.Climate.TAIGA:
+                    return Color.DarkKhaki;
+                case Biome.Climate.GRASSLAND:
+                    return Color.LightGreen;
+                case Biome.Climate.TEMPERATE_DECIDUOUS_FOREST:
+                    return Color.ForestGreen;
+                case Biome.Climate.TEMPERATE_RAIN_FOREST:
+                    return Color.LightSeaGreen;
+                case Biome.Climate.SUBTROPICAL_DESERT:
+                    return Color.OliveDrab;
+                case Biome.Climate.TROPICAL_SEASONAL_FOREST:
+                    return Color.SeaGreen;
+                case Biome.Climate.TROPICAL_RAIN_FOREST:
+                    return Color.DarkGreen;
+                
+                default:
+                    return Color.Black;
+            }
+        }
+
+        private Texture2D CreateBiomeTexture()
+        {
+            Texture2D texture = new Texture2D(GraphicsDevice, width, height);
+
+            Color[] data = new Color[width * height];
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color pixel = BiomeColor(biome.ClimateType(x, y));
+                    int offset = x + y * width;
+                    data[offset] = pixel;
+                }
+            }
+
+            texture.SetData(data);
+
+            return texture;
+        }
+
+        private Texture2D CreateGeologyTexture()
+        {
+            Texture2D texture = new Texture2D(GraphicsDevice, width, height);
+
+            Color[] data = new Color[width * height];
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color pixel = BiomeColor(biome.GeologyType(x, y));
+                    int offset = x + y * width;
+                    data[offset] = pixel;
+                }
+            }
+
+            texture.SetData(data);
+
+            return texture;
         }
 
         /// <summary>
@@ -62,9 +192,40 @@ namespace Rocks_for_Food
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            // Update world time
+
+            double elapsedMilliseconds = gameTime.ElapsedGameTime.TotalMilliseconds;
+            worldTime = worldTime.AddMilliseconds(elapsedMilliseconds * timeScale);
 
             base.Update(gameTime);
+
+            // Handle input events
+
+            MouseState mouseState = Mouse.GetState();
+
+            if (mouseState.ScrollWheelValue < previousMouseState.ScrollWheelValue)
+            {
+                zoomLevel *= 0.9f;
+            }
+            else if (mouseState.ScrollWheelValue > previousMouseState.ScrollWheelValue)
+            {
+                zoomLevel *= 1.1f;
+            }
+
+            // handle drag
+            if (GraphicsDevice.Viewport.Bounds.Contains(mouseState.Position) &&
+                mouseState.LeftButton.Equals(ButtonState.Pressed) && previousMouseState.LeftButton.Equals(ButtonState.Pressed))
+            {
+                panLevel.X += (mouseState.Position.X - previousMouseState.Position.X) / zoomLevel;
+                panLevel.Y += (mouseState.Position.Y - previousMouseState.Position.Y) / zoomLevel;
+            }
+
+            if (mouseState.RightButton == ButtonState.Pressed && previousMouseState.RightButton != ButtonState.Pressed)
+            {
+                showBiome = !showBiome;
+            }
+
+            previousMouseState = mouseState;
         }
 
         /// <summary>
@@ -75,7 +236,43 @@ namespace Rocks_for_Food
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            // Draw Game Map
+
+            var scale = Matrix.CreateScale(zoomLevel, zoomLevel, 1.0f);
+            var translate = Matrix.CreateTranslation(panLevel.X, panLevel.Y, 0);
+            var matrix = translate * scale;
+
+            spriteBatch.Begin(transformMatrix: matrix);
+
+            if (showBiome)
+            {
+                spriteBatch.Draw(biomeTexture, new Vector2(0, 0), Color.White);
+            }
+            else
+            {
+                spriteBatch.Draw(geologyTexture, new Vector2(0, 0), Color.White);
+            }
+
+            spriteBatch.End();
+
+            // Draw Static Content
+
+            string status = string.Format("Day {0} of Year {1}", worldTime.DayOfYear, worldTime.Year);
+            var mousePos = new Vector2(previousMouseState.Position.X, previousMouseState.Position.Y);
+            var worldPos = Vector2.Transform(mousePos, Matrix.Invert(matrix));
+
+            spriteBatch.Begin();
+
+            spriteBatch.DrawString(font, status, new Vector2(10, 10), Color.Black);
+            if (worldPos.X >= 0 && worldPos.Y >= 0 && worldPos.X < width && worldPos.Y < height)
+            {
+                var climate = biome.ClimateType((int)worldPos.X, (int)worldPos.Y);
+                var geology = biome.GeologyType((int)worldPos.X, (int)worldPos.Y);
+                string biomeString = string.Format("Climate: {0}, Geology: {1}", climate, geology);
+                spriteBatch.DrawString(font, biomeString, new Vector2(10, 30), Color.Black);
+            }
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
